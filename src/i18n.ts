45c89fb5c12221c0efc3511b5dab14dbff899fc1
gim_z66 <gimholg1992@foxmail.com>
2026-03-11 17:17:42 +0800

    redirect_to_https

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from "react-i18next";
 const zh = {
   "author": "作者",
   "date": "日期",
-  "main_title": "LF-WeMakit",
+  "main_title": "LF-Wemakit",
   "unavailable": "已停用",
   "dl_win_x64": "下载 Windows x64 应用",
   "open_in_browser": "浏览器中打开",
@@ -64,7 +64,7 @@ const zh = {
 const en = {
   "author": "Author",
   "date": "Date",
-  "main_title": "LF-WeMakit",
+  "main_title": "LF-Wemakit",
   "unavailable": "Unavailable",
   "dl_win_x64": "Download Window x64 Application",
   "open_in_browser": "Open in Browser",
